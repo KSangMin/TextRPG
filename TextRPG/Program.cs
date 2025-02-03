6ec78a5ed3dd8d50b779a143470578103587f32c
KSangMin <fhfpszmfnwm@naver.com>
2025-02-03 14:50:45 +0900

    상점 및 아이템 구매, 예외처리

--- TextRPG/Program.cs
+++ TextRPG/Program.cs
@@ -13,7 +13,6 @@
             Console.WriteLine("환영합니다!");
             while (true)
             {
-                Console.Clear();
                 game.Select();
             }
         }
@@ -26,11 +25,13 @@
             if (!rightInput)
             {
                 Console.WriteLine("입력이 잘못되었습니다. 다시 입력해주세요.");
+                Thread.Sleep(500);
                 return true;
             }
             if (select < minN || select > maxN)
             {
                 Console.WriteLine($"{minN}~{maxN}의 숫자를 입력해주세요.");
+                Thread.Sleep(500);
                 return true;
             }
             return false;
@@ -38,29 +39,29 @@
 
         class Character
         {
-            private Item[] items =
+            public List<Item> items = new List<Item>
             {
                 new Item("무쇠갑옷", false, 0, 5, "무쇠로 만들어져 튼튼한 갑옷입니다.")
                 , new Item("스파르타의 창", true, 7, 0, "스파르타의 전사들이 사용했다는 전설의 창입니다.")
                 , new Item("낡은 검", true, 2, 0, "쉽게 볼 수 있는 낡은 검입니다.")
             };
-            private int _level;
-            private string _name;
-            private string _classType;
-            private int _attack;
-            private int _defense;
-            private int _health;
-            private int _gold;
+            public int level;
+            public string name;
+            public string classType;
+            public int attack;
+            public int defense;
+            public int health;
+            public int gold;
 
             public Character(string name)
             {
-                _level = 1;
-                _name = name;
-                _classType = "전사";
-                _attack = 10;
-                _defense = 5;
-                _health = 100;
-                _gold = 1500;
+                level = 1;
+                this.name = name;
+                classType = "전사";
+                attack = 10;
+                defense = 5;
+                health = 100;
+                gold = 1500;
             }
 
             public void PrintState()
@@ -76,12 +77,13 @@
                 }
 
                 Console.Clear();
-                Console.WriteLine($"Lv. {_level:D2}");
-                Console.WriteLine($"{_name} ({_classType})");
-                Console.WriteLine($"공격력: {_attack}{(itemAttack > 0 ? $" (+{itemAttack})" : "")}");
-                Console.WriteLine($"방어력: {_defense}{(itemDefense > 0 ? $" (+{itemDefense})" : "")}");
-                Console.WriteLine($"체 력: {_health}");
-                Console.WriteLine($"Gold: {_gold}G");
+                Console.WriteLine("캐릭터의 정보가 표시됩니다.\n");
+                Console.WriteLine($"Lv. {level:D2}");
+                Console.WriteLine($"{name} ( {classType} )");
+                Console.WriteLine($"공격력: {attack}{(itemAttack > 0 ? $" (+{itemAttack})" : "")}");
+                Console.WriteLine($"방어력: {defense}{(itemDefense > 0 ? $" (+{itemDefense})" : "")}");
+                Console.WriteLine($"체 력: {health}");
+                Console.WriteLine($"Gold: {gold}G");
                 Console.WriteLine();
                 Console.WriteLine("0. 나가기");
 
@@ -93,8 +95,10 @@
             public void PrintInventory()
             {
                 Console.Clear();
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+                Console.WriteLine("해당 숫자 아이템을 선택해 착용/해제할 수 있습니다.\n");
                 Console.WriteLine("[아이템 목록]");
-                for(int i = 0; i < items.Length; i++)
+                for(int i = 0; i < items.Count; i++)
                 {
                     Item item = items[i];
                     Console.Write($"- {i + 1} ");
@@ -106,16 +110,32 @@
                 }
                 Console.WriteLine();
                 Console.WriteLine("0. 나가기");
-                Console.WriteLine("그 외. 해당 숫자 아이템 착용/해제하기");
 
                 int select = -1;
-                if (CheckWrongInput(ref select, 0, items.Length)) PrintInventory();
+                if (CheckWrongInput(ref select, 0, items.Count)) PrintInventory();
                 if (select == 0) return;
                 else
                 {
                     items[select - 1].isEquipped = !items[select - 1].isEquipped;
                     PrintInventory();
                 }
+            }
+
+            public bool UseGold(int price)
+            {
+                if (price > gold)
+                { 
+                    Console.WriteLine("Gold가 부족합니다.");
+                    Thread.Sleep(500);
+                    return false;
+                }
+                else
+                {
+                    gold -= price;
+                    Console.WriteLine("구매를 완료했습니다.");
+                    Thread.Sleep(500);
+                    return true;
+                } 
             }
         }
 
@@ -127,6 +147,7 @@
             public int attack;
             public int defense;
             public string desciption;
+            public int price = 0;
 
             public Item(string name, bool isWeapon, int attack, int defense, string description)
             {
@@ -136,10 +157,29 @@
                 this.defense = defense;
                 this.desciption = description;
             }
+
+            public Item(string name, bool isWeapon, int attack, int defense, string description, int price)
+            {
+                this.name = name;
+                this.isWeapon = isWeapon;
+                this.attack = attack;
+                this.defense = defense;
+                this.desciption = description;
+                this.price = price;
+            }
         }
 
         class Game
         {
+            private List<Item> _shop = new List<Item>
+            {
+                new Item("수련자 갑옷", false, 0, 5, "수련에 도움을 주는 갑옷입니다.", 1000)
+                , new Item("무쇠갑옷", false, 0, 9, "무쇠로 만들어져 튼튼한 갑옷입니다.", 2500)
+                , new Item("스파르타의 갑옷", false, 0, 15, "스파르타의 전사들이 사용했다는 전설의 갑옷입니다.", 3500)
+                , new Item("낡은 검", true, 2, 0, "쉽게 볼 수 있는 낡은 검입니다.", 600)
+                , new Item("청동 도끼", true, 5, 0, "어디선가 사용됐던 것 같은 도끼입니다.", 1500)
+                , new Item("스파르타의 창", true, 7, 0, "스파르타의 전사들이 사용했다는 전설의 창입니다.", 2100)
+            };
             private Character _character;
 
             public Game(Character character)
@@ -149,6 +189,7 @@
 
             public void Select()
             {
+                Console.Clear();
                 Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
                 Console.WriteLine();
                 Console.WriteLine("1. 상태 보기");
@@ -167,7 +208,57 @@
                         _character.PrintInventory();
                         break;
                     case 3:
+                        PrintShop();
                         break;
+                }
+            }
+
+            public void PrintShop()
+            {
+                Console.Clear();
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
+                Console.WriteLine("해당 숫자 아이템을 선택해 구매할 수 있습니다.\n");
+                Console.WriteLine("[보유 골드]");
+                Console.WriteLine(_character.gold + " G\n");
+                Console.WriteLine("[아이템 목록]");
+                for (int i = 0; i < _shop.Count; i++)
+                {
+                    Item item = _shop[i];
+                    Console.Write($"- {i + 1} {item.name,-10} | ");
+                    if (item.isWeapon) Console.Write($"공격력 +{item.attack} | ");
+                    else Console.Write($"방어력 +{item.defense} | ");
+                    bool isSelled = false;
+                    foreach (Item eq in _character.items)
+                    {
+                        if (eq.name == item.name)
+                        {
+                            isSelled = true;
+                            break;
+                        }
+                    }
+                    Console.Write($"{item.desciption, -15} | {(isSelled ? "구매완료" : $"{item.price} G")}\n");
+                }
+                Console.WriteLine();
+                Console.WriteLine("0. 나가기");
+
+                int select = -1;
+                if (CheckWrongInput(ref select, 0, _shop.Count)) PrintShop();
+                if (select == 0) return;
+                else
+                {
+                    bool isSelled = false;
+                    foreach (Item eq in _character.items)
+                    {
+                        if (eq.name == _shop[select - 1].name)
+                        {
+                            isSelled = true;
+                            Console.WriteLine("이미 구매한 아이템입니다.");
+                            Thread.Sleep(500);
+                            break;
+                        }
+                    }
+                    if(!isSelled && _character.UseGold(_shop[select- 1].price)) _character.items.Add(_shop[select - 1]);
+                    PrintShop();
                 }
             }
         }
